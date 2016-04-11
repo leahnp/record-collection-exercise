de35686f00c5f07f9a1d36dc87f5139c801b52c4
Leah <leahnpetersen@gmail.com>
2016-04-11 16:19:03 -0700

    added method to create table

--- lib/setup_db.rb
+++ lib/setup_db.rb
@@ -2,8 +2,33 @@ require 'sqlite3'
 
 class ReleaseDatabase
   attr_reader :db
-  
+
   def initialize(dbname = "releases")
-    @db = SQLite3::Database.new "database/#{dbname}.rb"
+    @db = SQLite3::Database.new "database/#{dbname}.db"
+
+  end
+
+  def reset_schema!
+
+    # label_code BLOB,artist TEXT,title TEXT,label TEXT,format TEXT,released INTEGER,date_added TEXT
+    # one table
+    query = <<-CREATESTATEMENT
+      CREATE TABLE albums (
+        id INTEGER PRIMARY KEY,
+        label_code BLOB,
+        artist TEXT NOT NULL,
+        title TEXT NOT NULL,
+        label TEXT,
+        format TEXT,
+        released INTEGER,
+        date_added TEXT
+        );
+      CREATESTATEMENT
+      # runs ONLY one query
+      @db.execute("DROP TABLE IF EXISTS albums;")
+      @db.execute(query)
   end
 end
+
+release_db = ReleaseDatabase.new
+release_db.reset_schema!
